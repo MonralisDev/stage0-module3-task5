16c72a22191b98788069a6fcab04b90d77ee92e0
M <M>
2025-01-25 13:22:08 +0200

    ++

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
-        int convertedMinutes = (int) (minutes * 60);
+        float convertedMinutes = (minutes * 60);
         System.out.println(convertedMinutes);
     }
 }
