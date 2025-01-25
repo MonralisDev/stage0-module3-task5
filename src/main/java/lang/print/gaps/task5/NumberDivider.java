ddb11ad0e7a7c465230f8f696a891eb64cd23893
M <M>
2025-01-25 13:24:27 +0200

    ++

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,6 +2,6 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
-        double result = toBeDivided / 5.0;
+        double result = (double) toBeDivided / 5.0;
     }
 }
